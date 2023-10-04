a3e42951c8945fd09bae9fa5fd1931092423ab2f
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2023-10-03 21:03:39 -0700

    Update Main_Menu.asm
    Added More PseudoCode, This Time For The "Character Select" Text Drawing In Lesson 4's Main Menu Handling Example.

--- Lesson_4/Main_Menu.asm
+++ Lesson_4/Main_Menu.asm
@@ -5,9 +5,9 @@
 ;=======================;
 ;   Define Variables:   ;
 ;=======================;
-cursor_flags                              = $6 ; 1 Byte
-character_selection_sprite_address 				= $7 ; 2 Bytes
-stage_selection_sprite_sprite_address     = $9 ; 2 Bytes
+cursor_flags				= $6 ; 1 Byte
+character_selection_sprite_address 	= $7 ; 2 Bytes
+stage_selection_sprite_sprite_address	= $9 ; 2 Bytes
 
 ;=======================;
 ; Set Sprite Addresses  ;
@@ -24,12 +24,45 @@ Main_Menu:
 	ld p3
 .Handle_Character_Selection_Text
 .Cursor_On_Character_Select
-  bn  Cursor_Flags, 0, .Cursor_Not_On_Character_Select ; Load Flag -- Is It Selected?
-  mov #<Highlighted>, character_selection_sprite_address+1
+  bn  Cursor_Flags, 0, .Cursor_Not_On_Character_Select ; Load Flag -- Is It Selected? If So, Which Character?
+.Character_1_Highlighted
+  ld Character_Flags
+  sub #1
+  bnz .Character_2_Highlighted
+  mov #<Highlighted_1>, character_selection_sprite_address+1
+  jmpf .Handle_Stage_Selection_Text
+.Character_2_Highlighted
+  ld Character_Flags
+  sub #2
+  bnz .Character_3_Highlighted
+  mov #<Highlighted_2>, character_selection_sprite_address+1
+  jmpf .Handle_Stage_Selection_Text
+.Character_3_Highlighted
+  ld Character_Flags
+  sub #3
+  bnz .Handle_Stage_Selection_Text ; Just In Case
+  mov #<Highlighted_3>, character_selection_sprite_address+1
   jmpf .Handle_Stage_Selection_Text
 .Cursor_Not_On_Character_Select  
-  mov #<Not_Highlighted>, character_selection_sprite_address+1
+.Character_1_Not_Highlighted
+  ld Character_Flags
+  sub #1
+  bnz .Character_2_Not_Highlighted
+  mov #<Not_Highlighted_1>, character_selection_sprite_address+1
+  jmpf .Handle_Stage_Selection_text
+.Character_2_Not_Highlighted
+  ld Character_Flags
+  sub #2
+  bnz .Character_3_Not_Highlighted
+  mov #<Not_Highlighted_2>, character_selection_sprite_address+1
+  jmpf .Handle_Stage_Selection_text
+.Character_1_Not_Highlighted
+  ld Character_Flags
+  sub #3
+  bnz  .Handle_Stage_Selection_text
+  mov #<Not_Highlighted_3>, character_selection_sprite_address+1
 .Handle_Stage_Selection_Text
+  ; ^ Repeat
 .Draw_Screen
 	P_Draw_Background_Constant Main_Menu_BackGround
 	P_Draw_Sprite character_selection_sprite_address, #8, #8
