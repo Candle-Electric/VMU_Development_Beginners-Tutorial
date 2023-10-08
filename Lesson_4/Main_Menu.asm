ab86dd3ecd860cf4ba8d4e39be3764338c857695
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2023-10-07 21:51:33 -0700

    Update Main_Menu.asm
    Added/Duplicated The Stage Selection Handling For Lesson 4's Main Menu Example.

--- Lesson_4/Main_Menu.asm
+++ Lesson_4/Main_Menu.asm
@@ -62,7 +62,44 @@ Main_Menu:
   bnz  .Handle_Stage_Selection_text
   mov #<Not_Highlighted_3>, character_selection_sprite_address+1
 .Handle_Stage_Selection_Text
-  ; ^ Repeat
+.Cursor_On_Stage_Select
+  bn  Cursor_Flags, 1, .Cursor_Not_On_Stage_Select
+.Stage_1_Highlighted
+  ld Stage_Flags
+  sub #1
+  bnz .Stage_2_Highlighted
+  mov #<Highlighted_1>, stage_selection_sprite_address+1
+  jmpf .Draw_Screen
+.Stage_2_Highlighted
+  ld Stage_Flags
+  sub #2
+  bnz .Stage_3_Highlighted
+  mov #<Highlighted_2>, stage_selection_sprite_address+1
+  jmpf .Draw_Screen
+.Stage_3_Highlighted
+  ld Stage_Flags
+  sub #3
+  bnz .Draw_Screen ; Just In Case
+  mov #<Highlighted_3>, stage_selection_sprite_address+1
+  jmpf .Draw_Screen
+.Cursor_Not_On_Stage_Select  
+.Stage_1_Not_Highlighted
+  ld Stage_Flags
+  sub #1
+  bnz .Stage_2_Not_Highlighted
+  mov #<Not_Highlighted_1>, stage_selection_sprite_address+1
+  jmpf .Draw_Screen
+.Stage_2_Not_Highlighted
+  ld Stage_Flags
+  sub #2
+  bnz .Stage_3_Not_Highlighted
+  mov #<Not_Highlighted_2>, stage_selection_sprite_address+1
+  jmpf .Draw_Screen
+.Stage_3_Not_Highlighted
+  ld Stage_Flags
+  sub #3
+  bnz  .Draw_Screen
+  mov #<Not_Highlighted_3>, stage_selection_sprite_address+1
 .Draw_Screen
 	P_Draw_Background_Constant Main_Menu_BackGround
 	P_Draw_Sprite character_selection_sprite_address, #8, #8
