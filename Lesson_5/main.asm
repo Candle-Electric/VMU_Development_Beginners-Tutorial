3c86b98bf2b1893d8eda279073894ee859e1c0f8
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2024-06-05 21:42:41 -0700

    Update main.asm
    Added More Of The Initialization For Main.ASM, Thanks To Kresna And Marcus And Sebastian, To The Start In Using For Lesson 5 Once we Start More Fully Coding It.

--- Lesson_5/main.asm
+++ Lesson_5/main.asm
@@ -15,6 +15,94 @@
 .include "./lib/libkcommon.asm"
 .include "./lib/sfr.i"
 
+;=======================;
+;  Include  Code Files  ;
+;=======================;
+; .include		"./Runner_Gameplay.asm"
+; .include		"./Main_Menu.asm"
+
+;=======================;
+;   Define Variables:   ;
+;=======================;
+p3_pressed				=		$4	; 1 Byte (For LibKCommon)
+p3_last_input			=		$5	; 1 Byte (For LibKCommon)
+character_flags			=		$17	; 1 Byte
+stage_flags				=		$18	; 1 Byte
+
+;=======================;
+; Initialize Variables: ;
+;=======================;
+; mov #0, character_flags
+; mov #0, stage_flags
+
+;=======================;
+;       Constants       ;
+;=======================;
+T_BTN_SLEEP				equ		7
+T_BTN_MODE				equ		6
+T_BTN_B1				equ		5
+T_BTN_A1				equ		4
+T_BTN_RIGHT1			equ		3
+T_BTN_LEFT1				equ		2
+T_BTN_DOWN1				equ		1
+T_BTN_UP1				equ		0
+
+;=======================;
+;  Prepare Application  ;
+;=======================;
+	.org	$00
+	jmpf	start
+
+	.org	$03
+	reti	
+
+	.org	$0b
+	reti	
+	
+	.org	$13
+	reti	
+
+	.org	$1b
+	jmpf	t1int
+	
+	.org	$23
+	reti	
+
+	.org	$2b
+	reti	
+	
+	.org	$33
+	reti	
+
+	.org	$3b
+	reti	
+
+	.org	$43
+	reti	
+
+	.org	$4b
+	clr1	p3int,0
+	clr1	p3int,1
+	reti
+
+	.org	$130	
+t1int:
+	push	ie
+	clr1	ie,7
+	not1	ext,0
+	jmpf	t1int
+	pop	ie
+	reti
+
+	.org	$1f0
+
+goodbye:	
+	not1	ext,0
+	jmpf	goodbye
+
+;=======================;
+;     Main Program      ;
+;=======================;
 start:
     player_sprite_y   = $19 ; 1 Byte
     player_y_velocity = $1a ; 1 Byte
