17ef8da86a8d5c02515cda6b4994db92f28b27a4
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-04-02 22:38:55 -0700

    Update Runner_Menu.asm
    Furthering The Skeleton Of The Code For The "Yet To Run" Menu.

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -2,9 +2,9 @@
 ;         Menu          ;
 ;=======================;
 Runner_Menu:
-	cursor_1			=		$6
-	cursor_2			=		$7
-
+	cursor_1_val			=		$6
+	cursor_2_val			=		$7
+	cursor_y_pos			=		$8
 Menu_Loop:
 	P_Draw_Sprite	cursor_sprite_address, b, c
 	ret
