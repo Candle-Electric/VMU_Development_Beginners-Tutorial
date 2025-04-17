415d9e3e91a697643dd3ac2e00b3782bd4e73abc
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-04-16 23:55:30 -0700

    Update Runner_Menu.asm
    Added A "LD P3" Button Here LibK Common Call For The Button Check In The "Infinite Runner Options Menu."

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -33,6 +33,7 @@ Menu_Loop:
 	callf Check_Button_Pressed
 	bn acc, 3, .Draw_Screen
 .Check_Buttons
+	ld p3
 .Draw_Screen
 	P_Draw_BackGround_Constant Menu_BG
 	P_Draw_Sprite	cursor_sprite_address, b, c
