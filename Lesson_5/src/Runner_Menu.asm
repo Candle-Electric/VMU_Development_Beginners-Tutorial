548782e210078df36c70b815200c18f54cdc6994
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-05-08 22:48:31 -0700

    Update Runner_Menu.asm
    Now Putting More Pseudo Code Returning On Pending Code Testing Later.

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -59,6 +59,7 @@ Menu_Loop:
 	; mov #8, c
 	P_Draw_Sprite	option1_sprite_address, b, c ; Move These To A Separate Line/The End Of Their Respective Functions, Since The X-Coordinates Will Change?
 	; .Option_2_2
+	; mov #8, b
 	; mov #16, c
 	; P_Draw_Sprite "" ""
 	P_Draw_Sprite	option2_sprite_address, b, c
