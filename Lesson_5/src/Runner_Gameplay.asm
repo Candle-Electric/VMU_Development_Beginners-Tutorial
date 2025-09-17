856e3d8efea7934d19675760b31fdc8e8a98ea60
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-09-16 22:33:22 -0700

    Update Runner_Gameplay.asm
    More Tabbing Again.

--- Lesson_5/src/Runner_Gameplay.asm
+++ Lesson_5/src/Runner_Gameplay.asm
@@ -10,8 +10,8 @@ Runner_Gameplay:
 	hundreds_digit					=	$1c
 	thousands_digit					=	$1d
 	score							=	$1e
-	digit_sprite_address 			= $1f
-	runner_jump_acceleration 		= $21
+	digit_sprite_address 			= 	$1f
+	runner_jump_acceleration 		= 	$21
 	; jump_accel_positive = $22
 	obstacle_sprite_address = $23
 	obstacle_sprite_x = $25
@@ -362,6 +362,7 @@ Digit_5
 ; 	st c
 ; 	callf Draw_Digit
 ; %end	
+
 
 
 
