38eb6fd2eb31ceb03b07a280baf886b5dedb6507
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-09-29 22:51:35 -0700

    Update Runner_Gameplay.asm
    Temp, Might Move The Rest.

--- Lesson_5/src/Runner_Gameplay.asm
+++ Lesson_5/src/Runner_Gameplay.asm
@@ -23,7 +23,7 @@ Runner_Gameplay:
 	dropping_obstacle_x 			=	$31
 	dropping_obstacle_y 			=	$32
 	player_animation_state 			= 	$33
-	dropping_obstacle_sprite_address = $34
+	dropping_obstacle_sprite_address =	$34
 	debugging_num = $35
 	Debugging_Number_Sprite_Address = $63
 
@@ -362,6 +362,7 @@ Digit_5
 ; 	st c
 ; 	callf Draw_Digit
 ; %end	
+
 
 
 
