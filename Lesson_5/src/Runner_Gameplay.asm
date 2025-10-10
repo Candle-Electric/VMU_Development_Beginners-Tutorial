f06cb8bb7353d6d51b88638c7c6738b7e0717a55
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-10-09 22:27:03 -0700

    Update Runner_Gameplay.asm
    Working On Paging For Response.

--- Lesson_5/src/Runner_Gameplay.asm
+++ Lesson_5/src/Runner_Gameplay.asm
@@ -14,7 +14,7 @@ Runner_Gameplay:
 	runner_jump_acceleration 		= 	$21		; 1 Byte
 	; jump_accel_positive 			= 	$22
 	obstacle_sprite_address 		= 	$23		; 2 Bytes
-	obstacle_sprite_x 				=	$25
+	obstacle_sprite_x 				=	$25		; 1 Byte
 	obstacle_sprite_y 				=	$26
 	frame_counter 					=	$27
 	collision_flags 				=	$28
@@ -362,6 +362,7 @@ Digit_5
 ; 	st c
 ; 	callf Draw_Digit
 ; %end	
+
 
 
 
