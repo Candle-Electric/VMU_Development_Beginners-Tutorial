64240cc96058cfc1170bf1cd23595006e764b80a
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-10-22 22:17:57 -0700

    Update Runner_Gameplay.asm
    Continuing Debugging Re: Number Notes.

--- Lesson_5/src/Runner_Gameplay.asm
+++ Lesson_5/src/Runner_Gameplay.asm
@@ -24,7 +24,7 @@ Runner_Gameplay:
 	dropping_obstacle_y 			=	$32		; 1 Byte
 	player_animation_state 			= 	$33		; 1 Byte
 	dropping_obstacle_sprite_address =	$34		; 2 Byte
-	debugging_num 					=	$35
+	debugging_num 					=	$35		; 1 Byte
 	Debugging_Number_Sprite_Address = 	$63
 
 ; Populate Character And Stage Flags
@@ -362,6 +362,7 @@ Digit_5
 ; 	st c
 ; 	callf Draw_Digit
 ; %end	
+
 
 
 
