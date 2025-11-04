1fa17f6aaed1c8cf10b8c945648cebdacd2abc97
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2025-11-03 23:28:01 -0800

    Update Runner_Gameplay.asm
    Here, Byte Additions.

--- Lesson_5/src/Runner_Gameplay.asm
+++ Lesson_5/src/Runner_Gameplay.asm
@@ -26,6 +26,7 @@ Runner_Gameplay:
 	dropping_obstacle_sprite_address =	$34		; 2 Byte
 	debugging_num 					=	$35		; 1 Byte
 	Debugging_Number_Sprite_Address = 	$63		; 2 Bytes
+	score_hi						=	$65		; 1 Byte
 
 ; Populate Character And Stage Flags
 	; ld cursor_flags
@@ -362,6 +363,7 @@ Digit_5
 ; 	st c
 ; 	callf Draw_Digit
 ; %end	
+
 
 
 
