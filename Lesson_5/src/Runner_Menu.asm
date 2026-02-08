fff3ee03131aa7e0b386ffcacd506182bb56ef32
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-02-08 05:28:56 +0000

    Refactor Runner_Menu.ASM With Updated Syntax
    Here's Bits Doing Continued Menu Work Through Options' Incrementing Code. SB&A,TAIGSAMBSBDR+AA!

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -36,6 +36,7 @@ Menu_Loop:
 	dec cursor_1_val ; "Option?"
 .Decrement_Option_2
 	sub #1
+    bnz . Check_Right
 ...
 .Check_Right
 	ld p3
@@ -79,6 +80,7 @@ jmpf .Draw_Screen
 	P_Draw_Sprite	cursor_sprite_address, b, c
 	P_Draw_Sprite	Confirm_Button_Sprite_Address, b, c ; Mov The "Highlighted" Or "Normal" Button To The Address.
 	ret
+
 
 
 
