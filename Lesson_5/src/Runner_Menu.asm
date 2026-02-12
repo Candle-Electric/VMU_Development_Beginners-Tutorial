343b98664c8b6fd300360bbe8bd863dd208151f0
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-02-11 23:58:14 -0600

    Refactor Runner_Menu.asm For Clarity And Structure
    Taking Vertical And Additional Option Calculating + Bolstering The Dot Relocation Headers' GeoLocations.

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -44,6 +44,7 @@ Menu_Loop:
 	mov #Button_Right, acc
 	callf Check_Button_Pressed
 	bn acc, 3, .Draw_Screen
+.Increment_Option_1
 .Check_Buttons ; D-Buttons + Up Top For Directions; Move Current Comment.
 .Check_B ; 
 	ld p3
@@ -81,6 +82,7 @@ jmpf .Draw_Screen
 	P_Draw_Sprite	cursor_sprite_address, b, c
 	P_Draw_Sprite	Confirm_Button_Sprite_Address, b, c ; Mov The "Highlighted" Or "Normal" Button To The Address.
 	ret
+
 
 
 
