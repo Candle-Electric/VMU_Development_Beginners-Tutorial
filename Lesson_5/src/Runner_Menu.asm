004db00ee6c9c62eb9873d876488ac8c05e7974b
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-02-14 19:53:07 -0600

    Update Runner_Menu.asm
    H10YIH,AP!+MASV&HBDC.

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -47,6 +47,7 @@ Menu_Loop:
 .Increment_Option_1
     ld cursor_y_Pos ; Do Consider Moving Before The Dot Header Jumper To Match The Above Syntax.
 	sub #1
+    bnz .Increment_Option_2
 .Check_Buttons ; D-Buttons + Up Top For Directions; Move Current Comment.
 .Check_B ; 
 	ld p3
@@ -84,6 +85,7 @@ jmpf .Draw_Screen
 	P_Draw_Sprite	cursor_sprite_address, b, c
 	P_Draw_Sprite	Confirm_Button_Sprite_Address, b, c ; Mov The "Highlighted" Or "Normal" Button To The Address.
 	ret
+
 
 
 
