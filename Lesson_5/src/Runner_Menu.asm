55eef9a6df54538c13b13ecfb49a05313dbd3b5e
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-02-24 23:57:30 -0600

    Update Runner_Menu.asm
    T.G.B.N.P.M.A.A.G.T.L.G.N.W.C.A.E.A.A.F.!

--- Lesson_5/src/Runner_Menu.asm
+++ Lesson_5/src/Runner_Menu.asm
@@ -90,8 +90,10 @@ jmpf .Draw_Screen
 	; P_Draw_Sprite "" ""
 	P_Draw_Sprite	option2_sprite_address, b, c
 	P_Draw_Sprite	cursor_sprite_address, b, c
+    mov #24, c
 	P_Draw_Sprite	Confirm_Button_Sprite_Address, b, c ; Mov The "Highlighted" Or "Normal" Button To The Address.
 	ret ; J.M.P.F. Main_Loop_Menu
+
 
 
 
