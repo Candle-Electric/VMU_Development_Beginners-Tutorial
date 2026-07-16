33808ca0ce65a1551d2e9b1b962d2a48abd54b59
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-07-15 23:01:59 -0700

    Refactor Main.ASM To Improve Structure And Clarity, Lining Up With TheCBarpsh's Improved Header Order.
    H.B.S., Thanks Again To TheCBarpsh! Adding More Code With Some Graphics Next.

--- Lesson_4/main.asm
+++ Lesson_4/main.asm
@@ -4,6 +4,59 @@
 ;=======================;
 
 ;=======================;
+;  Prepare Application  ;
+;=======================;
+	.org	$00
+	jmpf	start
+
+	.org	$03
+	reti	
+
+	.org	$0b
+	reti	
+	
+	.org	$13
+	reti	
+
+	.org	$1b
+	jmpf	t1int
+	
+	.org	$23
+	reti	
+
+	.org	$2b
+	reti	
+	
+	.org	$33
+	reti	
+
+	.org	$3b
+	reti	
+
+	.org	$43
+	reti	
+
+	.org	$4b
+	clr1	p3int,0
+	clr1	p3int,1
+	reti
+
+	.org	$130	
+t1int:
+	push	ie
+	clr1	ie,7
+	not1	ext,0
+	jmpf	t1int
+	pop	ie
+	reti
+
+	.org	$1f0
+
+goodbye:	
+	not1	ext,0
+	jmpf	goodbye
+
+;=======================;
 ;VMU Application Header ;
 ;=======================;
 .include "GameHeader.i"
@@ -86,59 +139,6 @@ T_BTN_RIGHT1				equ		3
 T_BTN_LEFT1				equ		2
 T_BTN_DOWN1				equ		1
 T_BTN_UP1				equ		0
-
-;=======================;
-;  Prepare Application  ;
-;=======================;
-	.org	$00
-	jmpf	start
-
-	.org	$03
-	reti	
-
-	.org	$0b
-	reti	
-	
-	.org	$13
-	reti	
-
-	.org	$1b
-	jmpf	t1int
-	
-	.org	$23
-	reti	
-
-	.org	$2b
-	reti	
-	
-	.org	$33
-	reti	
-
-	.org	$3b
-	reti	
-
-	.org	$43
-	reti	
-
-	.org	$4b
-	clr1	p3int,0
-	clr1	p3int,1
-	reti
-
-	.org	$130	
-t1int:
-	push	ie
-	clr1	ie,7
-	not1	ext,0
-	jmpf	t1int
-	pop	ie
-	reti
-
-	.org	$1f0
-
-goodbye:	
-	not1	ext,0
-	jmpf	goodbye
 
 ;=======================;
 ;     Main Program      ;
